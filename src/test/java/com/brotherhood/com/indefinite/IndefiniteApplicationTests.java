52f1fbe4c387142a045f9c49b5eda538fd815430
Goshgar <qosqarmirze@gmail.com>
2020-03-29 00:34:37 +0400

    Changes

---
+++ src/test/java/com/brotherhood/com/indefinite/IndefiniteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brotherhood.com.indefinite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IndefiniteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
